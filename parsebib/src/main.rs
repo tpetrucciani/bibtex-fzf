6487a7ace79917d4e1b173861f552046f3011a34
Tommaso Petrucciani <tommaso.petrucciani@gmail.com>
2018-08-07 21:00:13 +0200

    Program to write key/title pairs from BibTeX file.

---
+++ parsebib/src/main.rs
@@ -0,0 +1,35 @@
+extern crate nom_bibtex;
+
+use nom_bibtex::*;
+use std::fs;
+
+fn get_title<'a>(e: &'a Bibliography) -> &'a str {
+    for (k, v) in e.tags() {
+        if k == "title" {
+            return v;
+        }
+    }
+    panic!("Missing title in BibTeX entry")
+}
+
+fn entry_to_string(e: &Bibliography) -> String {
+    let key = e.citation_key();
+    let title = get_title(e);
+    return format!("{} -- {}", key, title);
+}
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return;
+    }
+    let filename = &args[1];
+    let text = fs::read_to_string(filename).expect("Unable to read file");
+    println!("{}", text);
+    let bibtex = Bibtex::parse(&text).unwrap();
+    let bib = bibtex.bibliographies();
+    for e in bib {
+        println!("{}", entry_to_string(e));
+    }
+}
